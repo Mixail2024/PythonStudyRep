2915b9fd83970bd63cff4813d6b971a697dba792
MIXAPC\Mihaly <mixailcsopik@gmail.com>
2024-05-20 14:19:26 +0200

    delete

--- 2_variables_task_2.py
+++
@@ -1,7 +0,0 @@
-import math
-a = float(input('Enter side a: '))
-b = float(input('Enter side b: '))
-c = float(input('Enter side c: '))
-hp = (a+b+c)/2
-s = math.sqrt(hp*(hp-a)*(hp-b)*(hp-c))
-print('S=',s)
--- 2_variables_task_3.py
+++
@@ -1,3 +0,0 @@
-import math
-r = float(input('Enter radius: '))
-print('l=', 2*math.pi*r)
